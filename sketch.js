f13ba60735f5013c827ae1b365fdc65243114424
subeenjeon <powersubeen@naver.com>
2014-09-11 14:29:35 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 function setup() {
 	createCanvas(640, 480);
 	background(212, 239, 231);
-	noFill();
-
+	
+	fill(255, 0, 0);
 	stroke(255, 0, 0);
 	rect(210, 210, 210, 210);
 
+	fill(0, 255, 0);
 	stroke(0, 255, 0);
 	ellipse(100, 100, 100, 100);
 }
